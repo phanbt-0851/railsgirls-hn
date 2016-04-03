8ed603a1bbdeb2a9414c4052fe63b3fac1984bf6
phanbt95 <phanbt_58@vnu.edu.vn>
2016-04-03 04:11:14 +0000

    add picture to comments, add like_Count

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -25,13 +25,14 @@ class CommentsController < ApplicationController
   # POST /comments.json
   def create
     @comment = Comment.new(comment_params)
-
+  @idea = Idea.find_by_id @comment.idea_id
+  @comments = @idea.comments.all
     respond_to do |format|
       if @comment.save
-        format.html { redirect_to @comment, notice: 'Comment was successfully created.' }
+        format.html { redirect_to idea_path(@comment.idea_id), notice: 'Comment was successfully created.' }
         format.json { render :show, status: :created, location: @comment }
       else
-        format.html { render :new }
+        format.html { render :template => "ideas/show"}
         format.json { render json: @comment.errors, status: :unprocessable_entity }
       end
     end
@@ -69,6 +70,6 @@ class CommentsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def comment_params
-      params.require(:comment).permit(:user_name, :body, :idea_id)
+      params.require(:comment).permit(:user_name, :body, :idea_id, :like_count, :picture)
     end
 end
--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,3 +1,5 @@
 class Comment < ActiveRecord::Base
     belongs_to :idea 
+    mount_uploader :picture, PictureUploader
+    validates_presence_of :user_name, :body
 end
