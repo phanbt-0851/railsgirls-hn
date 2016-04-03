b2cce9c1a7b382e776a0ed4fbc89157ed15cc6c3
phanbt95 <phanbt_58@vnu.edu.vn>
2016-04-03 03:01:19 +0000

    add comments

--- app/controllers/ideas_controller.rb
+++ app/controllers/ideas_controller.rb
@@ -1,6 +1,6 @@
 class IdeasController < ApplicationController
   before_action :set_idea, only: [:show, :edit, :update, :destroy]
-
+ 
   # GET /ideas
   # GET /ideas.json
   def index
@@ -10,6 +10,8 @@ class IdeasController < ApplicationController
   # GET /ideas/1
   # GET /ideas/1.json
   def show
+    @comments = @idea.comments.all
+  @comment = @idea.comments.build
   end
 
   # GET /ideas/new
